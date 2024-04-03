3dcb130a15659af645b6ddba6bae3ed25a6eb17d
Elias Ablan <eliasablan93@gmail.com>
2024-04-03 13:27:01 -0400

    add shadcn, geist and reformat layout

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,55 @@
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import type { Metadata } from 'next'
+
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/ThemeProvider'
+
+import './globals.css'
+import { Toaster } from '@/components/ui/sonner'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Elias Ablan',
+    default: 'Elias Ablan | Web and Technology',
+  },
+  description:
+    "I'm a web developer. I build apps and talk about web and tech.",
+  metadataBase: new URL(`https://${process.env.VERCEL_URL}/`),
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          // font
+          'text-primary font-sans antialiased',
+          GeistSans.variable,
+          GeistMono.variable,
+          // scrollbar
+          'scrollbar'
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster richColors expand closeButton />
+        </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  )
+}
