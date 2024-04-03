ec24837247912f88013e651301226398fdc74288
Elias Ablan <eliasablan93@gmail.com>
2024-04-03 16:42:22 -0400

    change motion movement for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from '@/components/ThemeToggler'
 const Footer = () => {
   return (
     <motion.div
-      initial={{ y: -100, opacity: 0 }}
+      initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       className="mx-auto hidden w-full max-w-2xl justify-center border-t-[1px] py-3 md:flex"
     >
