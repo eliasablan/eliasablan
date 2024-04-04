6e12da72bdff303bee033427ec36241a661611ed
Elias Ablan <eliasablan93@gmail.com>
2024-04-03 23:36:10 -0400

    Remove tag link from Blog component

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,8 +1,5 @@
 import PostItem from './PostItem'
 import { getPosts } from '@/sanity/queries'
-import Link from 'next/link'
-import { buttonVariants } from './ui/button'
-import { cn } from '@/lib/utils'
 
 const Blog = async () => {
   const posts = await getPosts()
@@ -11,29 +8,6 @@ const Blog = async () => {
     <div className="py-8 md:pt-12">
       <div className="flex items-center pb-3 text-xl font-medium">
         <h2 className="inline align-middle">Blog</h2>
-        <Link
-          className={cn(
-            buttonVariants({ variant: 'outline' }),
-            'ml-3 h-6 rounded-xl px-2'
-          )}
-          href="/tags"
-        >
-          <span>tags</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="ml-1 h-3 w-3"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-            />
-          </svg>
-        </Link>
       </div>
       <div className="w-full text-sm">
         {posts && posts.length > 0 ? (
