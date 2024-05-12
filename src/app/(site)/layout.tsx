c454ab7235dfe97b5c281ea118b19ac88165d37a
Elias Ablan <eliasablan93@gmail.com>
2024-05-12 17:31:30 -0400

    make scrollbar normal again

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           GeistSans.variable,
           GeistMono.variable,
           // scrollbar
-          'scrollbar-thin scrollbar-thumb-accent'
+          'scrollbar scrollbar-thumb-accent'
         )}
       >
         <ThemeProvider
