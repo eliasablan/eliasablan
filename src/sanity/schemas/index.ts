37731674aa7e463d9acbbfcbb5f5e422832cdbb2
Elias Ablan <eliasablan93@gmail.com>
2024-06-12 16:28:17 -0400

    remove sanity files and folder

--- src/sanity/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import home from './home'
-import settings from './settings'
-import projects from './projects'
-import posts from './posts'
-import tags from './tags'
-
-export const schemaTypes = [home, settings, projects, posts, tags]
